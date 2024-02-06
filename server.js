0162cae3c165f41e77e2a822f93eb9c3ce6cb172
Joshua Angulo <jangulo@albany.edu>
2024-02-06 00:14:31 -0500

    CORS configurations

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const fs = require('fs');
 const app = express();
 const port = 3030;
 
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: 'POST',
+};
+
 app.use(express.text());
 app.use(cors());
+app.use(cors(corsOptions));
 
 app.post('/executeJava', async(req, res) => {
   let javaCode = req.body;
