4aa0725d5ab8fe159cbc6b6a3f421cebcd413947
Joshua Angulo <jangulo@albany.edu>
2024-02-06 02:04:26 -0500

    Edited README and comments

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Java-Backend-Support
+# RunJava-code-at-backend
+
+## Build
+Run `node server.js` to run this project. 
+
+## Dependencies
+Node v20.11.0 
+Download: https://nodejs.org/en/download
+
+## Notes
+I essentially needed to test a project, so I create a backend that ran java code. Takes the context of the string, writes it in a .java file, then runs it on where it is being hosted on.
+Takes the name of the class and uses it as the name of the java file. 
+Cleans up by unlinking
+
+## How to connect
+- Send to url: `https://localhost:3030` or the url of where it is being hosted with the port of `3030`.
+- POST `/executeJava` (Full Url `https://localhost:3030/executeJava`)
+- Make sure the header is `text/plain` and that it gets sent as a string (Very Important).
+- Lastly, if you are hosting it remotely, update CORS with the url of the hosted server or it will get blocked by browser.
+
+## WARNING
+If you are using this for a live build, please update to prevent injections or add other protection for security reasons!
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* If you are using this for live please add preventions for Injections!!! */
 const express = require('express');
 const { exec } = require('child_process');
 const cors = require('cors');
@@ -6,6 +7,7 @@ const fs = require('fs');
 const app = express();
 const port = 3030;
 
+/* For Cross Origin Resource Sharing */
 const corsOptions = {
   origin: 'http://localhost:4200',
   methods: 'POST',
@@ -18,11 +20,13 @@ app.use(cors(corsOptions));
 app.post('/executeJava', async(req, res) => {
   let javaCode = req.body;
 
+  /* Currently only supports string */
   if (typeof javaCode !== 'string') {
     console.log('Invalid Java code provided.');
     return res.status(400).send('Invalid Java code provided.');
   }
 
+  /* Checks for empty java string */
   javaCode = javaCode.trim();
   if (!javaCode) {
     console.log('Empty Java code provided.');
@@ -32,26 +36,35 @@ app.post('/executeJava', async(req, res) => {
   const className = extractClassName(javaCode);
   const fileName = `${className}.java`;
   const fileName2 = `${className}.class`;
-  await fs.writeFileSync(fileName, javaCode);
+  await fs.writeFileSync(fileName, javaCode); // writes to a new file
 
+  /* Executes the run command for java */
   exec(`javac ${fileName} && java ${fileName.replace('.java', '')}`, async (error, stdout, stderr) => {
     console.log('Current Working Directory:', process.cwd());
-    await fs.unlinkSync(fileName);
+    await fs.unlinkSync(fileName); // removes the .java file
     if (error) {
       console.log(stderr);
+      /* Shortens the string and removes carrots for cleaner error messages */
       let response = stderr.substring(0, stderr.length - 10);
       res.send(response.replaceAll('^', ''));
     } else {
+      /* removes the .class file */
       await fs.unlinkSync(fileName2);
+      /* sends a response with the output */
       res.send(stdout);
     }
   });
 });
 
+/* Listens on port 3030 */
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
+/* 
+ * Function grabs the class name and returns it
+ * New Java versions allow no main methods so it defaults to main.java
+*/
 function extractClassName(javaCode) {
   const classRegex = /class\s+(\w+)/;
   const match = javaCode.match(classRegex);
